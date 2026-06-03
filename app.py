2737fd31d2f0005df85d4fc72bcd8189e7c16b97
T4M15 <temikotemi20@gmail.com>
2026-06-03 23:01:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,10 @@
-from flask import Flask, render_template
+from ext import app, db
+from models import Food
 
-app = Flask(__name__)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
 
-foods = [
-    {"type": "healthy", "disc": "Healthy food keeps your body strong and full of energy.", "img": "h1.jpg"},
-    {"type": "fastfood", "disc": "Fast food is tasty and quick, but often unhealthy if eaten too much.", "img": "f1.jpg"},
-    {"type": "fitness", "disc": "Protein-rich food helps build muscles and keeps you strong and active.", "img": "j1.jpg"}
-]
+    from routes import *
 
-
-@app.route("/")
-def home():
-    return render_template("home.html", foods=foods)
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/foodinfo")
-def foods_info():
-    return render_template("foods_info.html")
-
-@app.route("/foodinfo1")
-def foods_info1():
-    return render_template("foods_info1.html")
-
-@app.route("/foodinfo2")
-def foods_info2():
-    return render_template("foods_info2.html")
-
-
-app.run(debug=True)
+    app.run(debug=True)
